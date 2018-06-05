3f096a0b48565af5dab170be2ace62124d3c5eb3
김성박 <urstory@gmail.com>
2018-06-05 16:26:30 +0900

    jpaboard

--- jpaboard/src/main/java/examples/boot/jpaboard/domain/MemberRole.java
+++ jpaboard/src/main/java/examples/boot/jpaboard/domain/MemberRole.java
@@ -16,14 +16,14 @@ public class MemberRole {
     private String name;
 
     // MemberRole이 영속화될 때, Member도 영속화 시킨다. cascade = CascadeType.ALL
-    @ManyToOne(targetEntity = Member.class, cascade = CascadeType.ALL)
+    @ManyToOne(targetEntity = Member.class)
     @JoinColumn(name="member_id")
     private Member member;
 
     // helper
     public void setMember(Member member){
         this.member = member;
-        if(!member.getMemberRoles().contains(member)){
+        if(!member.getMemberRoles().contains(this)){
             member.getMemberRoles().add(this);
         }
     }
